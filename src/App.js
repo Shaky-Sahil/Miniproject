391d81b07e61e6a107df7816d395c882aff2568b
Abhinav Nair <abhinav0298@gmail.com>
2023-06-10 22:37:57 +0530

    User Profile added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, {Component, useState} from 'react'
 import { Loginform } from './components/Loginform'
 import { Register } from './components/Register'
+
 import "./App.css"
+import Userprofile from './components/Userprofile';
+import Location from './components/Location';
 
 function App() {
 
   return (
     <>
-    <div className='App'>     
-      <Loginform /> 
+    <div>     
+      <Location/>
     </div>
     </>
   );
---
+++ src/components/Userprofile.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import { useState,useRef } from 'react';
+import "./Userprofile.css"
+
+const Userprofile = () => {
+    
+    const [userProfile, setUserProfile] = useState({
+        name: 'John Doe',
+        username: 'johndoe',
+        email: 'johndoe@example.com',
+        profilePicUrl: 'https://example.com/profile-pic.jpg',
+      });
+    
+      const fileInputRef = useRef(null);
+    
+      const handlePictureClick = () => {
+        fileInputRef.current.click();
+      };
+    
+      const handlePictureChange = (event) => {
+        const file = event.target.files[0];
+        const reader = new FileReader();
+    
+        reader.onload = (e) => {
+          const newProfilePicUrl = e.target.result;
+          setUserProfile((prevProfile) => ({ ...prevProfile, profilePicUrl: newProfilePicUrl }));
+        };
+    
+        reader.readAsDataURL(file);
+      };
+    
+      return (  
+        
+        <div className="profile-screen">
+          <div className="profile-content">
+            <h1>Profile</h1>
+            <div className="profile-details">
+            <div className="profile-picture">
+              <div className="profile-picture-container" onClick={handlePictureClick}>
+                <img src={userProfile.profilePicUrl} alt="Profile Picture" />
+              </div>
+              <input type="file" accept="image/*" ref={fileInputRef} onChange={handlePictureChange} style={{ display: 'none' }} />
+            </div>
+              <h2>{userProfile.name}</h2>
+              <p>@{userProfile.username}</p>
+              <p>Email: {userProfile.email}</p>
+            </div>
+            <div>
+            <h1> &ensp;69 &ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp; 70</h1>
+            <h4>&ensp;Visited &ensp; &ensp; &ensp; &ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;Remaining</h4>
+            </div>
+            
+          </div>
+        </div>
+      );
+}
+
+export default Userprofile
