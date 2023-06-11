869cb12bb5471b1e894152140397235349edfc8b
YashNK <yashkamnani0@gmail.com>
2023-06-12 00:00:30 +0530

    locations add

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
     <>
  
     <div className='App'> 
-    {/* <Userprofile/> */}
+    {/* <Location/> */}
     <BrowserRouter>
     <Routes>
       <Route path='/'element={<Cardone/>}></Route>
